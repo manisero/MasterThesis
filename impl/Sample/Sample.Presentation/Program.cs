6f3e72f7c4204df3160e21c8d4188b076d79e3fb
manisero <michalaniserowicz@gmail.com>
2014-07-01 22:38:49 +0200

    Created Sample solution

---
+++ impl/Sample/Sample.Presentation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Sample.Presentation.Domain;
+
+namespace Sample.Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person
+                {
+                    FirstName = "John",
+                    LastName = "Doe"
+                };
+
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person.LastName);
+        }
+    }
+}
