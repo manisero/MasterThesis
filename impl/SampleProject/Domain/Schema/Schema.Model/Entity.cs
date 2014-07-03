e1099306614cd6762ea781ddd2a0353212f19066
manisero <michalaniserowicz@gmail.com>
2014-07-03 19:55:10 +0200

    Created Schema solution

---
+++ impl/SampleProject/Domain/Schema/Schema.Model/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Schema.Model
+{
+    public class Entity
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<Field> Fields { get; set; }
+    }
+}
---
+++ impl/SampleProject/Domain/Schema/Schema.Model/Field.cs
@@ -0,0 +1,9 @@
+﻿namespace Schema.Model
+{
+    public class Field
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+    }
+}
