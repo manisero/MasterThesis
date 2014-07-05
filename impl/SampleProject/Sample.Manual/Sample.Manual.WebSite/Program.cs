4016c9275c0dd2a8d20860077e18b488100ef093
manisero <michalaniserowicz@gmail.com>
2014-07-05 22:49:49 +0200

    Started creating sample manual web site

---
+++ impl/SampleProject/Sample.Manual/Sample.Manual.WebSite/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace Sample.Manual.WebSite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = new Uri("http://localhost:1234");
+            var configuration = new HostConfiguration
+                {
+                    UrlReservations = new UrlReservations
+                        {
+                            CreateAutomatically = true
+                        }
+                };
+
+            using (var host = new NancyHost(configuration, uri))
+            {
+                host.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+}
