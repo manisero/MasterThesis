c2814937453c7c45858be25325a95436a7aa4b33
manisero <michalaniserowicz@gmail.com>
2014-07-07 21:01:48 +0200

    Implemented updating Users

--- impl/SampleProject/Sample.Manual/dotnet/Sample.Manual.WebSite/Modules/Users/UsersModule.cs
+++ impl/SampleProject/Sample.Manual/dotnet/Sample.Manual.WebSite/Modules/Users/UsersModule.cs
@@ -1,18 +1,24 @@
 ﻿using Nancy;
 using Sample.Manual.DataAccess;
+using Sample.Manual.Domain.Events;
 using Sample.Manual.Domain.Views;
+using Nancy.ModelBinding;
+using Sample.Manual.Logic;
 
 namespace Sample.Manual.WebSite.Modules.Users
 {
     public class UsersModule : NancyModule
     {
         private readonly IRepository<User> _userRepository;
+        private readonly UserUpdatedEventHandler _handler;
 
-        public UsersModule(IRepository<User> userRepository) : base("/Users")
+        public UsersModule(IRepository<User> userRepository, UserUpdatedEventHandler handler) : base("/Users")
         {
             _userRepository = userRepository;
+            _handler = handler;
 
             Get["/{UserName}"] = Details;
+            Post["/{UserName}"] = Update;
         }
 
         public dynamic Details(dynamic parameters)
@@ -21,6 +27,19 @@ namespace Sample.Manual.WebSite.Modules.Users
             var user = _userRepository.SingleOrDefault(x => x.UserName == userName);
 
             return View[user];
+        }
+
+        public dynamic Update(dynamic parameters)
+        {
+            var user = this.Bind<Domain.Entities.User>();
+            var @event = new UserUpdatedEvent
+                {
+                    User = user
+                };
+
+            _handler.HandleEvent(@event);
+
+            return Details(parameters);
         }
     }
 }
